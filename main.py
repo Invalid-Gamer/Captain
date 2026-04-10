781375b0c6ed7013914b748f5fd6894f5a264d2c
ingressy <j.czinzoll@ingressy.de>
2026-04-10 10:38:18 +0200

    TCP

--- main.py
+++ main.py
@@ -1,14 +1,21 @@
 import time
+import threading
 
 from Sensoren import ADC
 from Motor import motors
+from Comm import Comm
 
 def main():
     print("===Testmode ===")
+    Comm.tcp_server_step()
     adc = ADC.ADC()
 
-    print(adc.get_ampere(0) + "A")
-    print(adc.get_12voltage(1) + "V")
+
+    t1 = threading.Thread(target=Comm.connhandler(), args=(adc, 0))
+    t1.start()
+
+    print(adc.get_ampere(0))
+    print(adc.get_12voltage(1))
 
 
     print("===Dreht Motoren in 5Sek!===")
