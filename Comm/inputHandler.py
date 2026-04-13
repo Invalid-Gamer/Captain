b4450a5ea4145a43664edd7a49bd891ab252501c
Invalid-Gamer <Invalid-Gamer@users.noreply.github.com>
2026-04-13 10:01:36 +0200

    Grr ichiuhvdsiudshds

--- Comm/inputHandler.py
+++ Comm/inputHandler.py
@@ -1,10 +1,9 @@
 import struct
-
-from Comm.Comm import active_tcp_connection
-from Motor.motors import vorwaerts
+from Comm import active_tcp_connection
+from Motor import motors
 
 def inputHandler(y):
     if active_tcp_connection:
         if y > 1950:
             movement_y = ((y - 1950) / (4096 - 1950)) * 100
-            vorwaerts(movement_y)
+            motors.vorwaerts(movement_y)
--- main.py
+++ main.py
@@ -3,21 +3,22 @@ import threading
 
 from Sensoren import ADC
 from Sensoren import Batterie_Prozent
-from Comm import inputHandler
+from Comm.Comm import udpHandler
 from Comm import Comm
 
 def main():
     adc = ADC.ADC()
     t1 = threading.Thread(target=Comm.connHandler, args=(adc,))
     t1.start()
-    t2 = threading.Thread(target=Comm.udpHandler)
-    t2.start()
-
+    #t2 = threading.Thread(target=Comm.udpHandler)
+    #t2.start()
     print(adc.get_ampere(0))
     print(adc.get_12voltage(1))
 
-    t3 = threading.Thread(target=Batterie_Prozent.collect_Bat_Prozent, args=(adc,))
-    t3.start()
+    #t3 = threading.Thread(target=Batterie_Prozent.collect_Bat_Prozent, args=(adc,))
+    #t3.start()
+    while True:
+        udpHandler()
 
 if __name__ == '__main__':
     main()
